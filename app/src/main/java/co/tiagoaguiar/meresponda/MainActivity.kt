eff7ea5ca19436b94bb996e9ee266b676cce2c9c
Tiago Aguiar <tiago.aguiar@moonjava.com.br>
2020-07-13 15:45:55 -0300

    parte 4 final

--- app/src/main/java/co/tiagoaguiar/meresponda/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/meresponda/MainActivity.kt
@@ -2,11 +2,25 @@ package co.tiagoaguiar.meresponda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    val res = arrayOf("Sim", "Não conte com isso!", "Talvez")
+    val random = Random.Default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if (editText.text.isEmpty()) {
+                textView.text = "Faça a sua pergunta"
+            } else {
+                val index = random.nextInt(3)
+                textView.text = res[index]
+            }
+        }
     }
 }
